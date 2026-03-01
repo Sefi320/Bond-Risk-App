a5b4c07a81a7a953d0de81693dd0ffb1329b5814
nurs3003 <nursulta@ualberta.ca>
2026-02-28 17:49:27 -0700

    adjusted global file for the feather file

--- Bond-Risk-App/global.R
+++ Bond-Risk-App/global.R
@@ -15,35 +15,9 @@ tickers <- c(
   "DGS30"   # 30-Year
 )
 
-from <- "1992-01-01"
-
-cmt_data <- tidyquant::tq_get(
-  tickers,
-  get = "economic.data",
-  from = from) %>% 
-  tidyr::fill(price,.direction = "down")
-
-
-cmt_clean <- cmt_data %>% 
-  dplyr::mutate(
-    maturity = case_when(
-      symbol == "DGS1MO" ~ 1/12,
-      symbol == "DGS3MO" ~ 3/12,
-      symbol == "DGS6MO" ~ 6/12,
-      symbol == "DGS1" ~ 1,
-      symbol == "DGS2" ~ 2,
-      symbol == "DGS5" ~ 5,
-      symbol == "DGS7" ~ 7,
-      symbol == "DGS10" ~ 10,
-      symbol == "DGS20" ~ 20,
-      symbol == "DGS30" ~ 30),
-    
-    rate = price / 100,
-    change_bps = (rate - dplyr::lag(rate)) * 10000,
-    m = 2,
-    price = 100
-  ) %>% 
-  tidyr::drop_na()
+cmt_clean <- read_feather(
+  "feather_data/cmt_clean.feather"
+)
 
 
 order <- c("DGS1MO", # 1-Month
